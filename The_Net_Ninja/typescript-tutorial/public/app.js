3e5d9686422b4ff6ad1933f4280b3d12408fa20c
Didrik147 <didrik.roest@yahoo.com>
2024-01-28 10:53:36 +0100

    l-13

--- The_Net_Ninja/typescript-tutorial/public/app.js
+++ The_Net_Ninja/typescript-tutorial/public/app.js
@@ -1,22 +1,31 @@
 "use strict";
 // Classes
 class Invoice {
-    constructor(c, d, a) {
+    /* readonly client: string;
+    private details: string;
+    public amount: number; */
+    /* constructor(c: string, d: string, a: number){
         this.client = c;
         this.details = d;
         this.amount = a;
+    } */
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes £${this.amount} for ${this.details}`;
     }
 }
-const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
-const invTwo = new Invoice('Luigi', 'Work on the Luigi website', 300);
+const invOne = new Invoice('Mario', 'work on the Mario website', 250);
+const invTwo = new Invoice('Luigi', 'work on the Luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = 'Yoshi';
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
 //console.log(form.children)
