ba8f4d8ac6bedc40983f3e0de9c5466507635cca
Didrik147 <didrik.roest@yahoo.com>
2024-01-28 09:47:17 +0100

    l-6

---
+++ The_Net_Ninja/typescript-tutorial/lesson-6.ts
@@ -0,0 +1,24 @@
+
+let age: any = 25;
+
+age = true;
+console.log(age)
+
+age = 'Hello'
+console.log(age)
+
+let mixed: any[]= [];
+
+mixed.push(5)
+mixed.push('Mario')
+mixed.push(false)
+console.log(mixed)
+
+let ninja: {name: any, age: any};
+
+ninja = {name: 'Yoshi', age: 25}
+console.log(ninja)
+
+
+ninja = {name: 30, age: 'Hello'}
+console.log(ninja)
