39d43e0e82ee6d85840668b8d0de36344759078b
Didrik147 <didrik.roest@yahoo.com>
2024-01-28 11:06:47 +0100

    l-14

--- The_Net_Ninja/typescript-tutorial/src/app.ts
+++ The_Net_Ninja/typescript-tutorial/src/app.ts
@@ -1,25 +1,4 @@
-// Classes
-class Invoice {
-    /* readonly client: string;
-    private details: string;
-    public amount: number; */
-
-    /* constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    } */
-
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number,
-    ){}
-
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoice.js'
 
 const invOne = new Invoice('Mario', 'work on the Mario website', 250);
 const invTwo = new Invoice('Luigi', 'work on the Luigi website', 300);
