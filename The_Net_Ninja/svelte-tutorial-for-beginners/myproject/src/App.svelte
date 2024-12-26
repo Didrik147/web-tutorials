88b3b391665c65c2cee27cfa77b2d24b224bf26c
Didrik Roest <didrik.roest@yahoo.com>
2024-12-26 22:26:03 +0100

    #3

--- The_Net_Ninja/svelte-tutorial-for-beginners/myproject/src/App.svelte
+++ The_Net_Ninja/svelte-tutorial-for-beginners/myproject/src/App.svelte
@@ -1,11 +1,16 @@
 <script>
-	export let name;
+	let name = 'Didrik';
+	let beltColour = 'Black';
+
+	const handleClick = () => {
+		beltColour = 'Orange'
+	};
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<h2>This is a test</h2>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>{beltColour} belt</p>
+	<button on:click={handleClick}>Update Belt Colour</button>
 </main>
 
 <style>
