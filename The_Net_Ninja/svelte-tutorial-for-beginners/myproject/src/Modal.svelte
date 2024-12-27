7c7fabcf9ea878acfc0fd1d007dd3794da0d9ced
Didrik Roest <didrik.roest@yahoo.com>
2024-12-27 12:41:57 +0100

    #13 done

--- The_Net_Ninja/svelte-tutorial-for-beginners/myproject/src/Modal.svelte
+++ The_Net_Ninja/svelte-tutorial-for-beginners/myproject/src/Modal.svelte
@@ -1,11 +1,16 @@
 <script>
+  // once - makes sure the event can only fire once (removes handler)
+  // preventDefault - prevent the default action (run e.preventDefault())
+  // self - only fires the event if the clicked element is the target
+
   export let message = "default value";
   export let showModal = false;
   export let isPromo = false;
 </script>
 
 {#if showModal}
-<div class="backdrop" class:promo={isPromo} on:click>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="backdrop" class:promo={isPromo} on:click|self >
   <div class="modal">
     <p>{message}</p>
   </div>
