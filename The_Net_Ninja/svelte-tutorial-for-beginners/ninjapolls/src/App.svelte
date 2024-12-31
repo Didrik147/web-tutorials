823544c573c86c2b2aba162642298bac6f7b5cab
Didrik Roest <didrik.roest@yahoo.com>
2024-12-31 11:59:27 +0100

    Done with #25

--- The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/App.svelte
+++ The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/App.svelte
@@ -1,17 +1,37 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import PollList from "./components/PollList.svelte";
 	import CreatePollForm from "./components/CreatePollForm.svelte";
 	import Tabs from "./shared/Tabs.svelte";
 
 	// Tabs
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
-	activeItem = 'Add New Poll';
 
 	const tabChange = (e) => {
 		activeItem = e.detail;
 	}
+
+	// Polls
+	let polls = [
+    {
+      id: 1,
+      question: 'Python or JavaScript?',
+      answerA: 'Python',
+      answerB: 'JavaScript',
+      votesA: 9,
+      votesB: 15,
+    },
+  ];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = 'Current Polls';
+	}
+
 </script>
 
 <Header />
@@ -19,9 +39,9 @@
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-		<p>Poll list component goes here</p>
+		<PollList {polls} />
 	{:else if activeItem === 'Add New Poll'}
-		<CreatePollForm />
+		<CreatePollForm on:add={handleAdd} />
 	{/if}
 </main>
 
