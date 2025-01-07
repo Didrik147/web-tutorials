d48920a5781824fcd3a7cd8af1cc4db4ee406a8f
Didrik Roest <didrik.roest@yahoo.com>
2025-01-07 14:10:36 +0100

    Done with #34

--- The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/components/PollDetails.svelte
+++ The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/components/PollDetails.svelte
@@ -2,13 +2,21 @@
   import Card from "../shared/Card.svelte";
   import PollStore from "../stores/PollStore";
   import Button from "../shared/Button.svelte";
+  import {tweened} from 'svelte/motion'
 
   export let poll;
 
   // Reactive values
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 * poll.votesA) / totalVotes);
-  $: percentB = Math.floor((100 * poll.votesB) / totalVotes);
+  $: percentA = Math.floor((100 * poll.votesA) / totalVotes) || 0;
+  $: percentB = Math.floor((100 * poll.votesB) / totalVotes) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+  //$: console.log($tweenedA, $tweenedB)
 
   // Handling votes
   const handleVote = (option, id) => {
@@ -41,12 +49,12 @@
     <p>Total votes: {totalVotes}</p>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" style="width: {percentA}%"></div>
+      <div class="percent percent-a" style="width: {$tweenedA}%"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%"></div>
+      <div class="percent percent-b" style="width: {$tweenedB}%"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
