72cf90787fdbe968927c8de8e5f64f7f5b3df3d1
Didrik Roest <didrik.roest@yahoo.com>
2025-01-07 13:22:03 +0100

    Done with #33

--- The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/components/PollList.svelte
+++ The_Net_Ninja/svelte-tutorial-for-beginners/ninjapolls/src/components/PollList.svelte
@@ -1,30 +1,14 @@
 <script>
-  //import { onDestroy, onMount } from 'svelte';
-  import PollStore from '../stores/PollStore.js'
+  import {fade, slide, scale} from 'svelte/transition';
+  import {flip} from 'svelte/animate';
+  import PollStore from "../stores/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
-  
-  //export let polls = [];
-
-  /* const unsub = PollStore.subscribe(data => {
-    polls = data;
-  }); */
-
-  /* onMount(() => {
-    // Maybe get data from a db
-    console.log('Component mounted');
-  }); */
-
-  /* onDestroy(() => {
-    // Unsub from store
-    console.log('Component destroyed');
-    unsub();
-  }); */
-
 </script>
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <!-- <div transition:fade> -->
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
       <PollDetails {poll} />
     </div>
   {/each}
@@ -35,5 +19,11 @@
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 20px;
+  }
+
+  @media (max-width: 576px) {
+    .poll-list {
+      grid-template-columns: 1fr;
+    }
   }
 </style>
