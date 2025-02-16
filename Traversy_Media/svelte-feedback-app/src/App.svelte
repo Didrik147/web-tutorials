db30edeb9fea97c0f98a284694ffb52563350a47
Didrik Roest <didrik.roest@yahoo.com>
2025-02-16 16:23:33 +0100

    20:23

--- Traversy_Media/svelte-feedback-app/src/App.svelte
+++ Traversy_Media/svelte-feedback-app/src/App.svelte
@@ -2,13 +2,47 @@
 	let firstName = 'Didrik'
 	let lastName = 'Roest'
 	let color = 'blue'
+	let showText = false
+	let users = [
+		{
+			id: '1',
+			name: 'Sonja'
+		},
+		{
+			id: '2',
+			name: 'Anette'
+		},
+		{
+			id: '3',
+			name: 'Niklas'
+		},
+	]
 
 	$: name = firstName + ' ' + lastName
+
+	const toggle = () => {
+		color = color === 'blue' ? 'red' : 'blue'
+		showText = !showText
+
+		users = [...users, {id: '4', name: 'Hans-Petter'}]
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 style='color: {color}'>Hello {name}!</h1>
+	{#if showText}
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>No text</p>
+	{/if}
+
+	<!-- <button on:click={() => color = 'purple'}>Click</button> -->
+	<button on:click={toggle}>Click</button>
+
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
+
 </main>
 
 <style>
