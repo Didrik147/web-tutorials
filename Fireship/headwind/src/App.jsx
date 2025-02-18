78d7d82db9f2901e06d5ce5b088517e8b296a54a
Didrik Roest <didrik.roest@yahoo.com>
2025-02-18 10:49:14 +0100

    Fireship

---
+++ Fireship/headwind/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SideBar from './SideBar'
+
+
+export default function App() {
+  return (
+    <div className='flex'>
+      <SideBar />
+    </div>
+  )
+}
--- Fireship/headwind/src/SideBar.jsx
+++ Fireship/headwind/src/SideBar.jsx
@@ -1,6 +1,10 @@
+
+
 const SideBar = () => {
   return (
-    <div className="fixed">
+    <div className="fixed top-0 left-0 h-screen w-16 m-0
+          flex flex-col
+          bg-gray-900 text-white shadow-lg">
       <i>A</i>
       <i>B</i>
       <i>C</i>
@@ -8,6 +12,6 @@ const SideBar = () => {
       <i>E</i>
     </div>
   );
-};
+}
 
 export default SideBar;
