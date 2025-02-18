516060070d7731a84fef2b502cd388d6e3c08553
Didrik Roest <didrik.roest@yahoo.com>
2025-02-18 11:13:30 +0100

    Pretty much done

--- Fireship/headwind/src/SideBar.jsx
+++ Fireship/headwind/src/SideBar.jsx
@@ -1,16 +1,27 @@
+import { BsPlus, BsFillLightningFill } from 'react-icons/bs';
+import { FaEnvelope, FaFire } from 'react-icons/fa';
 
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0
           flex flex-col
           bg-gray-900 text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaEnvelope size="20" />} />
     </div>
   );
 }
 
 export default SideBar;
+
+const SideBarIcon = ({ icon, text='tooltip 💡' }) => (
+  <div className="sidebar-icon group">
+    {icon}
+
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
+);
--- Fireship/headwind/tailwind.config.js
+++ Fireship/headwind/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: {
+          900: '#202225',
+          800: '#2f3136'
+        }
+      },
+    },
   },
   plugins: [],
 }
