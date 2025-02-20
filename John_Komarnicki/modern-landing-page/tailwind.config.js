77d14c195dea3d92249efee46255a7673f104073
Didrik147 <didrik.roest@yahoo.com>
2025-02-20 11:49:56 +0100

    44:53

--- John_Komarnicki/modern-landing-page/tailwind.config.js
+++ John_Komarnicki/modern-landing-page/tailwind.config.js
@@ -4,7 +4,15 @@
     "./public/**/*.{html, js}",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
     fontFamily: {
       Poppins: ['Poppins, sans-serif'],
     },
